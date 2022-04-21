93952178c86f630cf16484657e4a911f2e2e023f
NicholasGomis <nicholasgomis@icloud.com>
2022-04-21 17:19:30 +0100

    get function fully work

--- server/routes/record.js
+++ server/routes/record.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-
+import {deleteListingByName} from '../../demo.js'
 // recordRoutes is an instance of the express router.
 // We use it to define our routes.
 // The router will be added as a middleware and will take control of requests starting with path /listings.
@@ -9,8 +9,8 @@ const recordRoutes = Router();
 import { getDb } from '../db/conn.js';
 
 // This section will help you get a list of all the records.
-recordRoutes.route('/listings').get(async function (_req, res) {
-  const dbConnect = getDb();
+recordRoutes.get('/listings', async function (_req, res) {
+  const dbConnect = await getDb();
 
   dbConnect
     .collection('listingsAndReviews')
@@ -20,72 +20,72 @@ recordRoutes.route('/listings').get(async function (_req, res) {
       if (err) {
         res.status(400).send('Error fetching listings!');
       } else {
-        res.json(result);
+        res.json({
+          
+          success: true,
+          payload: result
+          
+        });
       }
     });
 });
 
-// This section will help you create a new record.
-recordRoutes.route('/listings/recordSwipe').post(function (req, res) {
-  const dbConnect = getDb();
-  const matchDocument = {
-    listing_id: req.body.id,
-    last_modified: new Date(),
-    session_id: req.body.session_id,
-    direction: req.body.direction,
-  };
+// // This section will help you create a new record.
+// recordRoutes.route('/listings/recordSwipe').post(function (req, res) {
+//   const dbConnect = getDb();
+//   const matchDocument = {
+//     listing_id: req.body.id,
+//     last_modified: new Date(),
+//     session_id: req.body.session_id,
+//     direction: req.body.direction,
+//   };
 
-  dbConnect
-    .collection('matches')
-    .insertOne(matchDocument, function (err, result) {
-      if (err) {
-        res.status(400).send('Error inserting matches!');
-      } else {
-        console.log(`Added a new match with id ${result.insertedId}`);
-        res.status(204).send();
-      }
-    });
-});
+//   dbConnect
+//     .collection('matches')
+//     .insertOne(matchDocument, function (err, result) {
+//       if (err) {
+//         res.status(400).send('Error inserting matches!');
+//       } else {
+//         console.log(`Added a new match with id ${result.insertedId}`);
+//         res.status(204).send();
+//       }
+//     });
+// });
 
 // This section will help you update a record by id.
-recordRoutes.route('/listings/updateLike').post(function (req, res) {
-  const dbConnect = getDb();
-  const listingQuery = { _id: req.body.id };
-  const updates = {
-    $inc: {
-      likes: 1,
-    },
-  };
+// recordRoutes.route('/listings/updateLike').post(function (req, res) {
+//   const dbConnect = getDb();
+//   const listingQuery = { _id: req.body.id };
+//   const updates = {
+//     $inc: {
+//       likes: 1,
+//     },
+//   };
 
-  dbConnect
-    .collection('listingsAndReviews')
-    .updateOne(listingQuery, updates, function (err, _result) {
-      if (err) {
-        res
-          .status(400)
-          .send(`Error updating likes on listing with id ${listingQuery.id}!`);
-      } else {
-        console.log('1 document updated');
-      }
-    });
-});
+//   dbConnect
+//     .collection('listingsAndReviews')
+//     .updateOne(listingQuery, updates, function (err, _result) {
+//       if (err) {
+//         res
+//           .status(400)
+//           .send(`Error updating likes on listing with id ${listingQuery.id}!`);
+//       } else {
+//         console.log('1 document updated');
+//       }
+//     });
+// });
 
 // This section will help you delete a record.
-recordRoutes.route('/listings/delete/:id').delete((req, res) => {
-  const dbConnect = getDb();
-  const listingQuery = { listing_id: req.body.id };
-
-  dbConnect
-    .collection('listingsAndReviews')
-    .deleteOne(listingQuery, function (err, _result) {
-      if (err) {
-        res
-          .status(400)
-          .send(`Error deleting listing with id ${listingQuery.listing_id}!`);
-      } else {
-        console.log('1 document deleted');
-      }
-    });
+recordRoutes.delete('/listings/:id', async function(req, res) {
+  const dbConnect = await getDb();
+  const id = Number(req.params.id)
+  const deleted = await deleteListingByName(dbConnect, id);
+     res.json({
+          
+          success: true,
+          payload: deleted
+          
+        });
 });
 
 export default recordRoutes;
