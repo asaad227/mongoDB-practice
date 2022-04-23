b83dbb42791278f5910527d0b45ec8db76f3ca10
NicholasGomis <nicholasgomis@icloud.com>
2022-04-23 18:42:17 +0100

    Patch / update working

--- server/routes/record.js
+++ server/routes/record.js
@@ -134,7 +134,7 @@ recordRoutes.delete('/listings/delete/:id', async (req, res)=> {
 
 
 recordRoutes.patch('/listings/update/:id', async (req, res) => {
-  // const updates = req.body
+
  
 const dbConnect = await getDb();
 const updated = await dbConnect.collection('listingsAndReviews').updateOne({_id: ObjectId(req.params.id)}, {$set: {newListing: req.body}}) 
